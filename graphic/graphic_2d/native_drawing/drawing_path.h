cd2548108d05d415c605c0991b489284ea24bf65
li-kiao <lx.96@foxmail.com>
2024-01-19 14:00:58 +0000

    新增SaveLayer、Path、Matrix、文字相关Drawing C 接口
    Signed-off-by: li-kiao <lixiang284@huawei.com>
    Change-Id: I875f87153cf86c832cf7663ee2510b8d19ffa26d

--- graphic/graphic_2d/native_drawing/drawing_path.h
+++ graphic/graphic_2d/native_drawing/drawing_path.h
@@ -44,6 +44,36 @@ extern "C" {
 #endif
 
 /**
+ * @brief Direction for adding closed contours.
+ *
+ * @since 11
+ * @version 1.0
+ */
+typedef enum {
+    /** clockwise direction for adding closed contours */
+    PATH_DIRECTION_CW,
+    /** counter-clockwise direction for adding closed contours */
+    PATH_DIRECTION_CCW,
+} OH_Drawing_PathDirection;
+
+/**
+ * @brief FillType of path
+ *
+ * @since 11
+ * @version 1.0
+ */
+typedef enum {
+    /** Specifies that "inside" is computed by a non-zero sum of signed edge crossings */
+    PATH_FILL_TYPE_WINDING,
+    /** Specifies that "inside" is computed by an odd number of edge crossings */
+    PATH_FILL_TYPE_EVEN_ODD,
+    /** Same as Winding, but draws outside of the path, rather than inside */
+    PATH_FILL_TYPE_INVERSE_WINDING,
+    /** Same as EvenOdd, but draws outside of the path, rather than inside */
+    PATH_FILL_TYPE_INVERSE_EVEN_ODD,
+} OH_Drawing_PathFillType;
+
+/**
  * @brief Creates an <b>OH_Drawing_Path</b> object.
  *
  * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
@@ -52,6 +82,17 @@ extern "C" {
  * @version 1.0
  */
 OH_Drawing_Path* OH_Drawing_PathCreate(void);
+
+/**
+ * @brief Creates an <b>OH_Drawing_Path</b> copy object.
+ *
+ * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
+ * @param OH_Drawing_Path Indicates the pointer to an <b>OH_Drawing_Rect</b> object.
+ * @return Returns the pointer to the <b>OH_Drawing_Path</b> object created.
+ * @since 12
+ * @version 1.0
+ */
+OH_Drawing_Path* OH_Drawing_PathCopy(OH_Drawing_Path*);
 
 /**
  * @brief Destroys an <b>OH_Drawing_Path</b> object and reclaims the memory occupied by the object.
@@ -138,6 +179,99 @@ void OH_Drawing_PathQuadTo(OH_Drawing_Path*, float ctrlX, float ctrlY, float end
  */
 void OH_Drawing_PathCubicTo(
     OH_Drawing_Path*, float ctrlX1, float ctrlY1, float ctrlX2, float ctrlY2, float endX, float endY);
+
+/**
+ * @brief Adds a new contour to the path, defined by the rect, and wound in the specified direction.
+ *
+ * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
+ * @param OH_Drawing_Path Indicates the pointer to an <b>OH_Drawing_Path</b> object.
+ * @param left Indicates the left coordinate of the upper left corner of the rectangle.
+ * @param top Indicates the top coordinate of the upper top corner of the rectangle.
+ * @param right Indicates the right coordinate of the lower right corner of the rectangle.
+ * @param bottom Indicates the bottom coordinate of the lower bottom corner of the rectangle.
+ * @param OH_Drawing_PathDirection Indicates the path direction.
+ * @since 12
+ * @version 1.0
+ */
+void OH_Drawing_PathAddRect(OH_Drawing_Path*, float left, float top, float right, float bottom, OH_Drawing_PathDirection);
+
+/**
+ * @brief Adds a new contour to the path, defined by the round rect, and wound in the specified direction.
+ *
+ * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
+ * @param OH_Drawing_Path Indicates the pointer to an <b>OH_Drawing_Path</b> object.
+ * @param OH_Drawing_RoundRect Indicates the pointer to an <b>OH_Drawing_RoundRect</b> object.
+ * @param OH_Drawing_PathDirection Indicates the path direction.
+ * @since 12
+ * @version 1.0
+ */
+void OH_Drawing_PathAddRoundRect(OH_Drawing_Path*, const OH_Drawing_RoundRect* roundRect, OH_Drawing_PathDirection);
+
+/**
+ * @brief Appends arc to path, as the start of new contour.Arc added is part of ellipse bounded by oval,
+ * from startAngle through sweepAngle. Both startAngle and sweepAngle are measured in degrees, where zero degrees 
+ * is aligned with the positive x-axis, and positive sweeps extends arc clockwise.If sweepAngle <= -360, or
+ * sweepAngle >= 360; and startAngle modulo 90 is nearly zero, append oval instead of arc. Otherwise, sweepAngle
+ * values are treated modulo 360, and arc may or may not draw depending on numeric rounding.
+ *
+ * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
+ * @param OH_Drawing_Path Indicates the pointer to an <b>OH_Drawing_Path</b> object.
+ * @param OH_Drawing_Rect Indicates the pointer to an <b>OH_Drawing_Rect</b> object.
+ * @param startAngle Indicates the starting angle of arc in degrees.
+ * @param sweepAngle Indicates the sweep, in degrees. Positive is clockwise.
+ * @since 12
+ * @version 1.0
+ */
+void OH_Drawing_PathAddArc(OH_Drawing_Path*, const OH_Drawing_Rect*, float startAngle, float sweepAngle);
+
+/**
+ * @brief Appends src path to path, transformed by matrix. Transformed curves may have different verbs,
+ * point, and conic weights.
+ *
+ * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
+ * @param OH_Drawing_Path Indicates the pointer to an <b>OH_Drawing_Path</b> object.
+ * @param src Indicates the pointer to an <b>OH_Drawing_Path</b> object.
+ * @param OH_Drawing_Matrix Indicates the length of the <b>OH_Drawing_Matrix</b> object.
+ * @since 12
+ * @version 1.0
+ */
+void OH_Drawing_PathAddPath(OH_Drawing_Path*, const OH_Drawing_Path* src, const OH_Drawing_Matrix*);
+
+/**
+ * @brief Return the status that point (x, y) is contained by path.
+ *
+ * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
+ * @param OH_Drawing_Path Indicates the pointer to an <b>OH_Drawing_Path</b> object.
+ * @param x Indicates the x-axis value of containment test.
+ * @param y Indicates the y-axis value of containment test.
+ * @return Returns true if the point (x, y) is contained by path.
+ * @since 12
+ * @version 1.0
+ */
+bool OH_Drawing_PathContains(OH_Drawing_Path*, float x, float y);
+
+/**
+ * @brief Transforms verb array, point array, and weight by matrix. transform may change verbs
+ * and increase their number. path is replaced by transformed data.
+ *
+ * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
+ * @param OH_Drawing_Path Indicates the pointer to an <b>OH_Drawing_Path</b> object.
+ * @param OH_Drawing_Matrix Indicates the pointer to an <b>OH_Drawing_Matrix</b> object.
+ * @since 12
+ * @version 1.0
+ */
+void OH_Drawing_PathTransform(OH_Drawing_Path*, const OH_Drawing_Matrix*);
+
+/**
+ * @brief Sets FillType, the rule used to fill path.
+ *
+ * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
+ * @param OH_Drawing_Path Indicates the pointer to an <b>OH_Drawing_Path</b> object.
+ * @param OH_Drawing_PathFillType Indicates the add path's fill type.
+ * @since 12
+ * @version 1.0
+ */
+void OH_Drawing_SetFillStyle(OH_Drawing_Path*, OH_Drawing_PathFillType);
 
 /**
  * @brief Closes a path. A line segment from the start point to the last point of the path is added.
