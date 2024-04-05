855dab342d64cf148789a00b9b5b5ae1f4c004c8
shilei <shilei91@huawei.com>
2024-04-05 05:36:19 +0000

    add property handler
    Signed-off-by: shilei <shilei91@huawei.com>
    Change-Id: I3f9c988aec4c3af15d5727205b3182f8d03da979

--- ark_runtime/jsvm/jsvm.h
+++ ark_runtime/jsvm/jsvm.h
@@ -2203,6 +2203,39 @@ JSVM_EXTERN JSVM_Status OH_JSVM_CloseInspector(JSVM_Env env);
 JSVM_EXTERN JSVM_Status OH_JSVM_WaitForDebugger(JSVM_Env env,
                                                 bool breakNextLine);
 
+/**
+ * @brief Define a JavaScript class with given class name, constructor, properties, callback handlers for
+ * property operations including get, set, delete, enum etc., and call as function callback.
+ *
+ * @param env: The environment that the API is invoked under.
+ * @param utf8name: Name of the JavaScript constructor function. For clarity, it is
+ * recommended to use the C++ class name when wrapping a C++ class.
+ * @param length: The length of the utf8name in bytes, or JSVM_AUTO_LENGTH if it
+ * is null-terminated.
+ * @param constructor: Struct include callback function that handles constructing instances of the class.
+ * When wrapping a C++ class, this method must be a static member with the JSVM_Callback.callback
+ * signature. A C++ class constructor cannot be used. 
+ * Include Optional data to be passed to the constructor callback as the data
+ * property of the callback info. JSVM_Callback provides more details.
+ * @param propertyCount: Number of items in the properties array argument.
+ * @param properties: Array of property descriptors describing static and instance data
+ * properties, accessors, and methods on the class See JSVM_PropertyDescriptor.
+ * @param propertyHandlerCfg: The instance object triggers the corresponding callback function.
+ * @param callAsFunctionCallback: Calling an instance object as a function will trigger this callback.
+ * @param result: A JSVM_Value representing the constructor function for the class.
+ * @return Returns JSVM_OK if the API succeeded.
+ * @since 12
+ */
+JSVM_EXTERN JSVM_Status OH_JSVM_DefineClassWithPropertyHandler(JSVM_Env env,
+                                                               const char* utf8name,
+                                                               size_t length,
+                                                               JSVM_Callback constructor,
+                                                               size_t propertyCount,
+                                                               const JSVM_PropertyDescriptor* properties,
+                                                               JSVM_PropertyHandlerCfg propertyHandlerCfg,
+                                                               JSVM_Callback callAsFunctionCallback,
+                                                               JSVM_Value* result);
+
 EXTERN_C_END
 
 /** @} */
